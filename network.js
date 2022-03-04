699dd1b3887c1d35b0d24068041eb1fba0f2e397
RamDevIO1 <ramdev.io.id@gmail.com>
2022-03-04 15:33:50 +0700

    .

--- network.js
+++ network.js
@@ -1,36 +1,27 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
-const { v4: uuidv4 } = require("uuid");
 const io = require("socket.io")(server);
-// Peer
 
-const { ExpressPeerServer } = require("peer");
-const peerServer = ExpressPeerServer(server, {
-  debug: true,
-});
 
 app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use("/peerjs", peerServer);
+app.use(express.static("views"));
 
 app.get("/", (req, rsp) => {
-  rsp.redirect(`/${uuidv4()}`);
+  res.render("index");
 });
 
-app.get("/:room", (req, res) => {
-  res.render("room", { roomId: req.params.room });
+app.get("/stream/:id", (req, res) => {
+  res.render("stream", { Id: req.params.id });
+});
+
+app.get("/watch/:id", (req, res) => {
+  res.render("watch", { Id: req.params.id });
 });
 
 io.on("connection", (socket) => {
-  socket.on("join-room", (roomId, userId) => {
-    socket.join(roomId);
-    socket.to(roomId).broadcast.emit("user-connected", userId);
-
-    socket.on("message", (message) => {
-      io.to(roomId).emit("createMessage", message);
-    });
-  });
+  console.log(`Made socket connection with ${socket.id}}`);
+  
 });
 
 server.listen(process.env.PORT || 3030);
